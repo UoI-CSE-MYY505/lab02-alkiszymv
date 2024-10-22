b8da87d8c32e508b9ce1ceb8d55c932f6e131579
alkiszymv <183715039+alkiszymv@users.noreply.github.com>
2024-10-22 16:46:01 +0300

    Update lab02.s

--- lab02.s
+++ lab02.s
@@ -10,8 +10,19 @@ array: .word 1, 0, 1, 12, 0, 1, 4
     li a2, 1
 prog:
 #-----------------------------
-# Write your code here!
-# Do not remove the prog label or write code above it!
+    add s0, zero, zero
+loop:    
+    beq  s0, a1, notFound
+    slli t0, s0, 2
+    add  t0, t0, a0
+    lw   t1, 0(t0)
+    beq  t1, a2, nFound
+    addi s0, s0, 1
+    j    loop
+notFound:
+    addi s0, zero, -1
+nFound:
+    add  s0, t1, zero
 #-----------------------------
 done:
     addi a7, zero, 10 
